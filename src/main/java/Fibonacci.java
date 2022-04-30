c07a79aaaf6b1030d1ffe63c7c2b50cba8da9b89
shane51 <suse51@163.com>
2022-04-30 13:13:09 +0800

    pass tests with small number

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,8 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Fibonacci {
+
+    static Map<Integer, Integer> dp = new HashMap<>();
+
     public int getFb(int n) {
+        if(n==0){
+            return 0;
+        }
         if(n== 1 || n== 2){
             return 1;
         }
-        return 0;
+        if(Objects.nonNull(dp.get(n))){
+            return dp.get(n);
+        }
+
+        int fbn = getFb(n-1) + getFb(n-2);
+        dp.put(n, fbn);
+        return fbn;
     }
 }
